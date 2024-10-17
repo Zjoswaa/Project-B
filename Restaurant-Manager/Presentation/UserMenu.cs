8336282707dc0fc966885aa8ed5476eaa888ccaa
Zjoswaa <joshuakijkuit@hotmail.com>
2024-10-17 12:47:59 +0200

    Improve login menu, fix authenticator bug.

--- Restaurant-Manager/Presentation/UserMenu.cs
+++ Restaurant-Manager/Presentation/UserMenu.cs
@@ -1,57 +1,55 @@
 ﻿using Spectre.Console;
 
-namespace Restaurant_Manager.Presentation
+
+class UserMenu
 {
-    internal static class UserMenu
+    public static void ShowUserMenu(ref bool loggedIn)
     {
-        public static void ShowUserMenu(ref bool loggedIn)
+        while (loggedIn)
         {
-            while (loggedIn)
+            Console.Clear();
+
+            AnsiConsole.Write(new Rule("[yellow]Reservation Menu[/]"));
+
+            var userSelectionPrompt = new SelectionPrompt<string>()
+                .Title("[cyan]Please select an option:[/]")
+                .AddChoices(new[] { "Make a Reservation", "View Reservation", "Edit Reservations", "Remove Reservation", "View Menu", "Logout" });
+
+            var userSelection = AnsiConsole.Prompt(userSelectionPrompt);
+
+            switch (userSelection)
             {
-                Console.Clear();
-
-                AnsiConsole.Write(new Rule("[yellow]Reservation Menu[/]"));
-
-                var userSelectionPrompt = new SelectionPrompt<string>()
-                    .Title("[cyan]Please select an option:[/]")
-                    .AddChoices(new[] { "Make a Reservation", "View Reservation", "Edit Reservations", "Remove Reservation", "View Menu", "Logout" });
-
-                var userSelection = AnsiConsole.Prompt(userSelectionPrompt);
-
-                switch (userSelection)
-                {
-                    case "Make a Reservation":
-                        Console.WriteLine("TBA...");
-                        Thread.Sleep(1000);
-                        Console.Clear();
-                        break;
-                    case "View Reservation":
-                        Console.WriteLine("TBA...");
-                        Thread.Sleep(1000);
-                        Console.Clear();
-                        break;
-                    case "Edit Reservations":
-                        Console.WriteLine("TBA...");
-                        Thread.Sleep(1000);
-                        Console.Clear();
-                        break;
-                    case "Remove Reservation":
-                        Console.WriteLine("TBA...");
-                        Thread.Sleep(1000);
-                        break;
-                    case "View Menu":
-                        Console.WriteLine("TBA...");
-                        Thread.Sleep(1000);
-                        Console.Clear();
-                        break;
-                    case "Logout":
-                        loggedIn = false;
-                        AnsiConsole.MarkupLine("[red]Logging out...[/]");
-                        Thread.Sleep(1000);
-                        Console.Clear();
-                        Program.ShowMainMenu();
-                        break;
-                }
+                case "Make a Reservation":
+                    Console.WriteLine("TBA...");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    break;
+                case "View Reservation":
+                    Console.WriteLine("TBA...");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    break;
+                case "Edit Reservations":
+                    Console.WriteLine("TBA...");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    break;
+                case "Remove Reservation":
+                    Console.WriteLine("TBA...");
+                    Thread.Sleep(1000);
+                    break;
+                case "View Menu":
+                    Console.WriteLine("TBA...");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    break;
+                case "Logout":
+                    loggedIn = false;
+                    AnsiConsole.MarkupLine("[red]Logging out...[/]");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    MainMenuPresentation.ShowMainMenu();
+                    break;
             }
         }
     }
