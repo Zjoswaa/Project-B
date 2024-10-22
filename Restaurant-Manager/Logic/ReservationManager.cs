2e2834406ab9d3a74056e80358a8fd99c9d50cbb
badr <1078106@hr.nl>
2024-10-22 15:31:09 +0200

    Added getall methods for tables, removed availableslots stuff

--- Restaurant-Manager/Logic/ReservationManager.cs
+++ Restaurant-Manager/Logic/ReservationManager.cs
@@ -1,3 +1,25 @@
-class ReservationManager //: IReservation
+class ReservationManager
 {
+    public int GetSelectedLocationID(string locName)
+    {
+        Dictionary<int, string> locations = Database.GetAllLocations();
+        int locID = 0;
+
+        foreach (KeyValuePair<int, string> kvp in locations)
+        {
+            if (locName == kvp.Value)
+            {
+                locID = kvp.Key;
+            }
+        }
+
+        return locID;
+    }
+
+    public void CreateReservation(int locID, string timeslot, DateTime date, int groupsize)
+    {
+        Dictionary<List<object>, int> reservations = Database.GetAllReservations();
+
+        //Database.InsertReservationsTable();
+    }
 }
