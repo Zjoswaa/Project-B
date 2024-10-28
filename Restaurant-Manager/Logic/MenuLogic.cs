d190e114214d9b88a9ab5fd23df59afa1a1fa6dd
333suki <sogaandmomayez@gmail.com>
2024-10-28 17:47:23 +0100

    made a lil start to MenuLogic

--- Restaurant-Manager/Logic/MenuLogic.cs
+++ Restaurant-Manager/Logic/MenuLogic.cs
@@ -2,5 +2,37 @@
 
 class MenuLogic
 {
-    // WIP
+    public static void AddDish()
+    {
+        Console.WriteLine("Enter the dish name:");
+        string dishname = Console.ReadLine();
+
+        double price;
+        while (true)
+        {
+            Console.WriteLine("Enter the dish price:");
+            string dishprice = Console.ReadLine();
+
+            if (double.TryParse(dishprice, out price))
+            {
+                break; // if price is valid, break out the loop
+            }
+            else
+            {
+                Console.WriteLine("Invalid price. Please enter a valid number.");
+            }
+        }
+
+        Console.WriteLine("Is the dish vegan? (yes/no)");
+        string input = Console.ReadLine().Trim().ToLower();
+        bool isVegan = false;
+        if (input == "yes")
+        {
+            isVegan = true;
+        }
+        else if (input != "no")
+        {
+            Console.WriteLine("Invalid input. Please enter 'yes' or 'no'.");
+        }
+    }
 }
