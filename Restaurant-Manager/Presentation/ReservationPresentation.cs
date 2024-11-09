66bf3baf0b8a56f86c23b8c1a4b9947432b95544
Young $heldon <1078106@hr.nl>
2024-11-09 02:16:23 +0100

    refactored reservation menu code

--- Restaurant-Manager/Presentation/ReservationPresentation.cs
+++ Restaurant-Manager/Presentation/ReservationPresentation.cs
@@ -1,8 +1,5 @@
 using Spectre.Console;
 
-
-// TO FIX: weird string formatting before giving input for group size
-// TO FIX: garbage if statements
 public static class ReservationPresentation
 {
     public static void Present()
@@ -10,6 +7,16 @@ public static class ReservationPresentation
         ReservationManager resManager = new();
 
         long userID = State.LoggedInUser.ID;
+        long locID = UserLocation(resManager);
+        string timeslot = UserTimeslot(resManager);
+        int groupsize = UserGroupSize();
+
+        //resManager.CreateReservation(userID, locID, timeslot, date, groupsize);
+    }
+
+    static long UserLocation(ReservationManager resManager)
+    {
+        Console.CursorVisible = false;
 
         // User selects a Location, of which the ID gets stored
         var locationChoice = AnsiConsole.Prompt(
@@ -17,57 +24,63 @@ public static class ReservationPresentation
             .Title("[cyan]Select a location:[/]")
             .AddChoices(resManager.LocationNamesToList()));
 
-        long locID = resManager.GetSelectedLocationID(locationChoice);
+        return resManager.GetSelectedLocationID(locationChoice);
+    }
+
+    static string UserTimeslot(ReservationManager resManager)
+    {
+        Console.CursorVisible = false;
 
         // Timeslot object gets converted into a list of times in string format, user can pick one
         var timeslotChoice = AnsiConsole.Prompt(
             new SelectionPrompt<string>()
             .Title("[cyan]Select a timeslot:[/]")
             .AddChoices(resManager.TimeslotsToList()));
-        string timeslot = timeslotChoice;
+        
+        return timeslotChoice;
+    }
+
+    static int UserGroupSize()
+    {
+        Console.CursorVisible = false;
+        bool isSelected = false;
+        int currentOption = 1;
+        int groupsize = 0;
 
         // Creates a Spectre panel in which the user can pick the amount of people for a reservation
-        Console.CursorVisible = false;
-        string Buffer = "Reservation for ^\n1\nv people"; // Define an empty string as buffer
+        string Buffer = $"\n         ^ \nReservation for {currentOption} person\n";
         Panel Panel = new(new Text($"Enter the amount of people for the reservation using the arrow keys:\n{Buffer}\n").Centered()); // Define the Panel and Text within it
         Panel.Expand = true; // Panel takes full width
 
         AnsiConsole.Clear();
         AnsiConsole.Write(Panel); // Render it
 
-        bool isSelected = false;
-        int currentOption = 1;
-        int groupsize = 0;
-
         while (!isSelected)
         {
             ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
 
-            if (currentOption != 7 && keyInfo.Key == ConsoleKey.UpArrow)
+            Buffer = (currentOption != 6) ? $"\n         ^ \nReservation for {currentOption} person\n" : $"\n         \nReservation for {currentOption} people\n         v";
+
+            if (currentOption != 6 && keyInfo.Key == ConsoleKey.UpArrow)
             {
                 currentOption += 1;
-                Buffer = $"         ^ \nReservation for {currentOption} people\n         v";
-            }
-            if (currentOption != 0 && keyInfo.Key == ConsoleKey.DownArrow)
-            {
-                currentOption -= 1;
-                Buffer = $"         ^ \nReservation for {currentOption} people\n         v";
+                Buffer = (currentOption != 6) ? $"\n         ^ \nReservation for {currentOption} people\n         v" : $"\n         \nReservation for {currentOption} people\n         v";
             }
 
-            if (currentOption == 7 && keyInfo.Key == ConsoleKey.UpArrow)
+            if (currentOption != 1 && keyInfo.Key == ConsoleKey.DownArrow)
             {
-                currentOption = 1;
-                Buffer = $"         ^ \nReservation for {currentOption} person\n         v";
-            }
-            if (currentOption == 0 && keyInfo.Key == ConsoleKey.DownArrow)
-            {
-                currentOption = 6;
-                Buffer = $"         ^ \nReservation for {currentOption} people\n         v";
+                currentOption -= 1;
+                Buffer = (currentOption != 1) ? $"\n         ^ \nReservation for {currentOption} people\n         v" : $"\n         ^ \nReservation for {currentOption} person\n";
             }
 
             if (keyInfo.Key == ConsoleKey.Enter)
             {
                 groupsize = currentOption;
+                isSelected = true;
+            }
+
+            if (keyInfo.Key == ConsoleKey.Backspace)
+            {
                 break;
             }
 
@@ -77,6 +90,6 @@ public static class ReservationPresentation
                 AnsiConsole.Write(Panel); // Re-render the panel with the updated text
         }
 
-        ///resManager.CreateReservation(userID, locID, timeslot, date, groupsize);
+        return groupsize;
     }
 }
