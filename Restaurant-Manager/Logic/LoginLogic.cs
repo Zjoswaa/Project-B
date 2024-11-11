43225bae307fa09b45677f9a08681defc6a3744b
Zjoswaa <joshuakijkuit@hotmail.com>
2024-11-11 17:41:47 +0100

    Fix database handle issue

--- Restaurant-Manager/Logic/LoginLogic.cs
+++ Restaurant-Manager/Logic/LoginLogic.cs
@@ -1,9 +1,9 @@
 ﻿public static class LoginLogic {
-    public static bool VerifyPassword(string Username, string Password) {
-        if (!Database.UsersTableContainsUser(Username)) {
+    public static bool VerifyPassword(string Email, string Password) {
+        if (!Database.UsersTableContainsUser(Email)) {
             return false;
         }
         //Console.WriteLine(Password == Encryptor.Decrypt(Database.GetEncryptedPassword(Username)!));
-        return Password == Encryptor.Decrypt(Database.GetEncryptedPassword(Username)!);
+        return Password == Encryptor.Decrypt(Database.GetEncryptedPassword(Email)!);
     }
 }
